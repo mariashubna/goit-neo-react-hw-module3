4bfc29258171d2e9727a537297ab65c83b1c1a5e
mariashubna <mariashubna@gmail.com>
2024-12-01 16:16:53 +0100

    created

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,49 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+import { useId } from "react";
+import css from "./ContactForm.module.css";
+
+const validateSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  number: Yup.string()
+    .matches(
+      /^\d{3}-\d{2}-\d{2}$/,
+      "Invalid phone number format (expected: 333-33-33)"
+    )
+    .required("Required"),
+});
+
+const initialValues = {
+  name: "",
+  number: "",
+};
+
+const ContactForm = ({ handleSubmit }) => {
+  const nameId = useId();
+  const numberId = useId();
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validateSchema}
+    >
+      <Form className={css.form}>
+        <label htmlFor={nameId}>Name</label>
+        <Field type="text" id={nameId} name="name" />
+        <ErrorMessage name="name" component="span" />
+        <label htmlFor={numberId}>Number</label>
+        <Field type="string" id={numberId} name="number" />
+        <ErrorMessage name="number" component="span" />
+        <button className={css.btn} type="submit">
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,12 @@
+import css from "./SearchBox.module.css";
+
+const SearchBox = ({ searchValue, onSearch }) => {
+  return (
+    <label className={css.search}>
+      Find contacts by name
+      <input type="text" value={searchValue} onChange={onSearch} />
+    </label>
+  );
+};
+
+export default SearchBox;
